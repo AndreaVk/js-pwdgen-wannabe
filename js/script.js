4c65b66a87b5fa64533e84b89e472e61023d6ada
AndreaVk <vico.ac1996@gmail.com>
2021-11-04 16:58:49 +0100

    bozza esercizio

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// ========= TEST ALTERT JS ==========
+
+// alert ('Hello world'); 
+
+let myName = prompt('Inserisci il tuo nome');
+document.getElementById('myName').innerHTML = myName;
+console.log(myName);
+
+let lastName = prompt('Inserisci il tuo cognome');
+document.getElementById('lastName').innerHTML = lastName;
+console.log(lastName);
+
+let favCol = prompt('Inserisci il tuo colore preferito');
+document.getElementById('favCol').innerHTML = favCol;
+console.log(favCol);
+
+const pwGen = myName + lastName + favCol + 21;
+document.getElementById('pwGen').innerHTML = pwGen;
+console.log(pwGen); 
